5844e1987a89e76dc440a8c163e134b2cc482ee1
David Ellsworth <david@worms2d.info>
2014-02-28 23:45:47 -0800

    Slight optimizations to test script

--- test-multiplication-regex.sh
+++ test-multiplication-regex.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-regex=$(cat "regex for matching multiplication.txt"|tr -d '[:space:]')
+regex=$(tr -d '[:space:]' < "regex for matching multiplication.txt")
 
 for (( a=1; a<=12; a++ )); do
 for (( b=1; b<=12; b++ )); do
 for (( c=1; c<=144; c++ )); do
 	result=$(
-		echo "$(perl -E "print 'x' x $a; print '*'; print 'x' x $b; print '='; print 'x' x $c")" |
-		pgrep "$regex"
+		perl -E "print 'x' x $a; print '*'; print 'x' x $b; print '='; print 'x' x $c" |
+			pgrep "$regex"
 	)
 	if [ -n "$result" ]; then
 		echo $a \* $b = $c
