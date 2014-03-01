24834d4eb33d115b20014db1ac14d2c4dbb43dc1
David Ellsworth <david@worms2d.info>
2014-03-01 01:36:25 -0800

    test script: print time taken to process regex

--- test-multiplication-regex.sh
+++ test-multiplication-regex.sh
@@ -5,12 +5,18 @@ regex=$(tr -d '[:space:]' < "regex for matching multiplication.txt")
 for (( a=1; a<=12; a++ )); do
 for (( b=1; b<=12; b++ )); do
 for (( c=1; c<=144; c++ )); do
-	result=$(
-		perl -E "print 'x' x $a; print '*'; print 'x' x $b; print '='; print 'x' x $c" |
-			pgrep "$regex"
-	)
-	if [ -n "$result" ]; then
-		echo $a \* $b = $c
+	result=$( (
+		time (perl -E "print 'x' x $a; print '*'; print 'x' x $b; print '='; print 'x' x $c" |
+			pgrep "$regex")
+	) 2>&1 )
+	grep_result="${result%%
+*}"
+	time_result="${result#*
+}"
+	time_result="${time_result%%
+*}"
+	if [ -n "${grep_result}" ]; then
+		echo $time_result: $a \* $b = $c
 	fi
 done
 done
