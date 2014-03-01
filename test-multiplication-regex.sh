2d2edd24074c144041d61ab64854f3194a324b6e
David Ellsworth <david@worms2d.info>
2014-02-28 23:29:17 -0800

    Now works for all positive values including 1

--- test-multiplication-regex.sh
+++ test-multiplication-regex.sh
@@ -2,8 +2,8 @@
 
 regex=$(cat "regex for matching multiplication.txt"|tr -d '[:space:]')
 
-for (( a=2; a<=12; a++ )); do
-for (( b=2; b<=12; b++ )); do
+for (( a=1; a<=12; a++ )); do
+for (( b=1; b<=12; b++ )); do
 for (( c=1; c<=144; c++ )); do
 	result=$(
 		echo "$(perl -E "print 'x' x $a; print '*'; print 'x' x $b; print '='; print 'x' x $c")" |
