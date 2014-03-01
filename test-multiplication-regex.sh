dceda845c9841e8fdc2f65266aed517e437c6e02
David Ellsworth <david@worms2d.info>
2014-02-28 23:10:49 -0800

    Initial commit - multiplication regex works for a*b=c if a>=2 && b>=2

---
+++ test-multiplication-regex.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+regex=$(cat "regex for matching multiplication.txt"|tr -d '[:space:]')
+
+for (( a=2; a<=12; a++ )); do
+for (( b=2; b<=12; b++ )); do
+for (( c=1; c<=144; c++ )); do
+	result=$(
+		echo "$(perl -E "print 'x' x $a; print '*'; print 'x' x $b; print '='; print 'x' x $c")" |
+		pgrep "$regex"
+	)
+	if [ -n "$result" ]; then
+		echo $a \* $b = $c
+	fi
+done
+done
+done
