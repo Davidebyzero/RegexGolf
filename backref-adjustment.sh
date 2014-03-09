5f0e19d8105b150e4d65dbde39603ef2846444f7
David Ellsworth <david@worms2d.info>
2014-03-08 18:07:27 -0800

    Backreference adjustment script: Add a feature to read and overwrite the same file

--- backref-adjustment.sh
+++ backref-adjustment.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
 if [ -z "$3" ] || [ "$3" -eq 0 ] || [ "$1" -gt "$2" ]; then
-    echo "Usage: $0 FIRST LAST INC"
-    echo "Adjust all regex backreferences starting with FIRST and ending with LAST by INC."
-    echo "Example: $0 3 15 -1 < INPUTFILE > OUTPUTFILE"
-    echo '         This changes all backreferences from \3 to \15 to go from \2 to \14 instead.'
+    echo -n "\
+\
+Usage: $0 FIRST LAST INC [FILE]
+Add INC to the index of all regex backreferences from FIRST to LAST inclusive.
+Example: $0 3 15 -1 < INPUTFILE > OUTPUTFILE
+         This changes all backreferences in INPUTFILE from \3 to \15 to go from
+         \2 to \14 instead, and writes the result to OUTPUTFILE.
+Example: $0 5 23 3 regex.txt
+         This changes all backreferences in "regex.txt" from \5 to \23 to go from
+         '\8 to \26 instead, and overwrites the file "regex.txt" with the result.
+"
     exit
 fi
 
@@ -20,4 +27,18 @@ script=$(
     fi
     echo -n 'print'
 )
-perl -ne"$script"
+
+if [ -z "$4" ]; then
+    perl -ne"$script"
+else
+    if [ -f "$4" ]; then
+        input=$(cat "$4")
+        if [ "$?" -eq 0 ]; then
+            echo -n "$input" | perl -ne"$script" > "$4"
+        else
+            echo "Error read file $4"
+        fi
+    else
+        echo "Error opening file $4"
+    fi
+fi
