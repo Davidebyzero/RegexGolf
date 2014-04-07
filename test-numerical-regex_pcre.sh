8bc7e0699335011138213bb6f863a171cd70ee0d
David Ellsworth <david@worms2d.info>
2014-04-07 04:08:58 -0700

    Add general numerical regex testing script

---
+++ test-numerical-regex_pcre.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+if [ -z "$3" ]; then
+	echo "Usage: $0 FILE FIRST LAST"
+	echo 'Reads a regex matching something in the domain "^x*$" from FILE and feeds it all'
+    echo 'numbers in the range FIRST to LAST, printing the ones that match.'
+	exit
+fi
+
+regex=$(tr -d '[:space:]' < "$1" | sed -e 's/(?#[^)]*)//g')
+
+echo Regex length = $(echo -n "$regex"|wc -c)
+echo Regex md5sum = $(echo -n "$regex"|md5sum)
+echo
+
+perl -E '
+	foreach my $a ('"$2"'..'"$3"')  {
+		say "x" x ($a);
+	}
+	' |
+# too small a --buffer-size will result in falsely detected false positives and negatives
+pcregrep --buffer-size=2M --line-buffered --match-limit=1000000000 -e"$regex" -e'^@' |
+while read -r a
+do
+	echo ${#a}
+done
---
+++ test-numerical-regex_perl.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+if [ -z "$3" ]; then
+	echo "Usage: $0 FILE FIRST LAST"
+	echo 'Reads a regex matching something in the domain "^x*$" from FILE and feeds it all'
+    echo 'numbers in the range FIRST to LAST, printing the ones that match.'
+	exit
+fi
+
+regex=$(tr -d '[:space:]' < "$1" | sed -e 's/(?#[^)]*)//g')
+
+echo Regex length = $(echo -n "$regex"|wc -c)
+echo Regex md5sum = $(echo -n "$regex"|md5sum)
+echo
+
+echo -n "$regex" |
+perl -E '
+	$regex = <>;
+	$n = 0;
+	$z = 1;
+	foreach my $a ('"$2"'..'"$3"')  {
+		@match = ("x" x $a) =~ /$regex/;
+		if (@match) {
+			say $a # . " -> " . length(@match[0]);
+		}
+	}
+'
